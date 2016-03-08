5d20e6b6506727a96f2041ee2c00fdad0fd83489
Rogerio Araujo <rogerio.araujo@gmail.com>
2016-03-08 14:48:11 -0300

    Initial project push

---
+++ main.cpp
@@ -0,0 +1,12 @@
+
+#include <QApplication>
+
+#include "screenshot.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Screenshot screenshot;
+    screenshot.show();
+    return app.exec();
+}
