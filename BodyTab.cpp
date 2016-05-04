4d0947caabd74f1ecc5232771b395e9cb64e255c
Rogerio Araujo <rogerio.araujo@gmail.com>
2016-05-04 11:23:18 -0300

    Adding support to response formatting

--- BodyTab.cpp
+++ BodyTab.cpp
@@ -1,12 +1,16 @@
 #include <QtWidgets>
 #include <QStringList>
+#include <QDomDocument>
 
 #include "BodyTab.h"
 
 BodyTab::BodyTab()
 {
+    createFormmatLayout();
+
     QVBoxLayout *mainLayout = new QVBoxLayout;
     responseEditor = new QTextEdit;
+    mainLayout->addLayout(formatLayout);
     mainLayout->addWidget(responseEditor);
     setLayout(mainLayout);
 }
@@ -18,6 +22,68 @@ void BodyTab::clear()
 
 void BodyTab::processBody(QByteArray body, QString mimeType)
 {
-    QString content = QString::fromUtf8(body.data());
-    responseEditor->setPlainText(content);
+    responseMimeType = mimeType;
+    responseContent = QString::fromUtf8(body.data());
+
+    this->prettyFormat();
+}
+
+void BodyTab::prettyFormat()
+{
+    if(responseMimeType.contains("application/json"))
+    {
+        QJsonDocument doc = QJsonDocument::fromJson(responseContent.toUtf8());
+        QString formattedString = doc.toJson(QJsonDocument::Indented);
+        responseEditor->setPlainText(formattedString);
+    }
+    else if(responseMimeType.contains("text/xml"))
+    {
+        QDomDocument doc("xml");
+        doc.setContent(responseContent);
+        QString formattedString = doc.toString(4);
+        responseEditor->setPlainText(formattedString);
+    }
+    else
+    {
+        responseEditor->setPlainText(responseContent);
+    }
+
+    prettyButton->setChecked(true);
+    rawButton->setChecked(false);
+}
+
+void BodyTab::rawFormat()
+{
+    responseEditor->setPlainText(responseContent);
+
+    prettyButton->setChecked(false);
+    rawButton->setChecked(true);
+}
+
+void BodyTab::createFormmatLayout()
+{
+    prettyButton = createButton(tr("Pretty"), this, SLOT(prettyFormat()));
+    rawButton = createButton(tr("Raw"), this, SLOT(rawFormat()));
+
+    QStringList formats;
+    formats << "JSON" << "XML" << "HTML" << "TEXT";
+    formatsCombo = new QComboBox;
+    formatsCombo->addItems(formats);
+
+    formatLayout = new QHBoxLayout;
+    formatLayout->addWidget(prettyButton);
+    formatLayout->addWidget(rawButton);
+    formatLayout->addWidget(formatsCombo);
+    formatLayout->addStretch();
+}
+
+QPushButton *BodyTab::createButton(const QString &text, QWidget *receiver,
+                                      const char *member)
+{
+    QPushButton *button = new QPushButton(text);
+    button->connect(button, SIGNAL(clicked()), receiver, member);
+    button->setMinimumWidth(100);
+    button->setCheckable(true);
+    button->setChecked(false);
+    return button;
 }
--- BodyTab.h
+++ BodyTab.h
@@ -5,6 +5,9 @@
 #include <QWidget>
 
 class QTextEdit;
+class QHBoxLayout;
+class QPushButton;
+class QComboBox;
 
 class BodyTab : public QWidget
 {
@@ -15,9 +18,23 @@ public:
     void clear();
     void processBody(QByteArray body, QString mimeType);
 
+private slots:
+    void prettyFormat();
+    void rawFormat();
+
 private:
+    void createFormmatLayout();
+    QPushButton *createButton(const QString &text, QWidget *receiver, const char *member);
 
     QTextEdit *responseEditor;
+    QString responseContent;
+    QString responseMimeType;
+
+    QHBoxLayout *formatLayout;
+
+    QPushButton *prettyButton;
+    QPushButton *rawButton;
+    QComboBox *formatsCombo;
 };
 
 #endif // BODYTAB_H
